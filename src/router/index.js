b2b8760ff23b5903ad268c79c664cc21101b793d
weitian1993gbrsa <weitian1993.gbrsa@gmail.com>
2026-01-17 22:30:51 +0800

    #

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
-
 import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '../views/LoginView.vue'
 import TesterView from '../views/TesterView.vue'
 
 const router = createRouter({
