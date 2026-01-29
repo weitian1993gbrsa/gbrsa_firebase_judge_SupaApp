9a35662c36d088504dbf70966b05f594ce3676fa
weitian1993gbrsa <weitian1993.gbrsa@gmail.com>
2026-01-29 22:40:57 +0800

    a

--- src/views/LiveScoreboardView.vue
+++ src/views/LiveScoreboardView.vue
@@ -515,11 +515,11 @@ h1 { font-weight: 900; letter-spacing: 0.1em; color: white; font-size: 3rem; mar
 
 /* 8 STATIONS (4x2) */
 .grid-8 { grid-template-columns: repeat(4, 1fr); grid-template-rows: 1fr 1fr; }
-.grid-8 .score-val { font-size: 60cqmin; }
+.grid-8 .score-val { font-size: 55cqmin; }
 
 /* 12 STATIONS (4x3) */
 .grid-12 { grid-template-columns: repeat(4, 1fr); grid-template-rows: repeat(3, 1fr); }
-.grid-12 .score-val { font-size: 70cqmin; }
+.grid-12 .score-val { font-size: 73cqmin; }
 
 /* MODAL STYLES */
 .modal-backdrop {
